345855fdb7835ba7d9228f36471ac5aa7bb744f3
Enzomniac <rhsupton@gmail.com>
2019-09-30 18:26:37 +0200

    base content

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="view">
+        <div class="grid-item info">
+            MENU
+        </div>
+        <div class="grid-item main">
+            NEW INVOICE
+        </div>
+        <div class="grid-item sub-main">
+            QUOTE
+        </div>
+    </div>
+</body>
+</html>
