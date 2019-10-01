532ded4772ff5d010ce968306732b1cef804c154
Enzomniac <rhsupton@gmail.com>
2019-10-01 12:47:42 +0200

    Invoice view

--- script.js
+++ script.js
@@ -31,25 +31,28 @@ function mainView() {
 function invoiceView() {
     let theHtml = "";
     theHtml = `
-        <div class="grid-item info">
-            Invoice:<br>
-            Select Client
-        </div>
-        <div class="grid-item main" onclick="changeView(mainView())">
-            NEW INVOICE
-        </div>
-        <div class="grid-item sub-main">
-            QUOTE
-        </div>
-        <div class="grid-item sub-one" onclick="changeView(mainView())">
-            << BACK
-        </div>
-        <div class="grid-item sub-two">
-            SUB TWO
-        </div>
-        <div class="grid-item sub-three">
-            SUB THREE
-        </div>
+    <div class="grid-item info">
+        Invoice:<br>
+        Select Client
+    </div>
+    <div class="grid-item main subQuad">
+        <div class="quad-item">A - G</div>
+        <div class="quad-item">H - N</div>
+        <div class="quad-item">O - W</div>
+        <div class="quad-item">X - Z</div>
+    </div>
+    <div class="grid-item sub-main">
+        GENERIC
+    </div>
+    <div class="grid-item sub-one" onclick="changeView(mainView())">
+        << BACK
+    </div>
+    <div class="grid-item sub-two">
+        .
+    </div>
+    <div class="grid-item sub-three">
+        NEW CLIENT
+    </div>
     `;        
     return theHtml;
 }
