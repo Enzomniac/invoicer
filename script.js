f0fddcac45b4249ea513f1e13536c3e1b083f503
Enzomniac <rhsupton@gmail.com>
2019-10-02 10:32:46 +0200

    Client List

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+let STATE = {
+    clientData: 'HELLO'
+}
+
+console.log(STATE.clientData);
+
+
 changeView(mainView());
+
+loadJson();
+console.log(STATE.clientData);
+
+function loadJson() {
+    fetch('storage/client.json').then(
+        function(response) {
+            if (response.status !== 200) {
+                console.log("Error: " + response.status);
+                return;
+            }
+            response.json().then(function(data) {                
+                STATE.clientData = data.clientData;
+            });
+        }
+    );
+}
 
 function changeView(theView) {
     document.getElementById('view').innerHTML = theView;
@@ -28,31 +52,42 @@ function mainView() {
     return theHtml;
 }
 
+function checkClientData() {
+    console.log("Here we Go");
+    let htmlString = "";
+    for (i = 0; i < STATE.clientData.length; i++) {
+        console.log(STATE.clientData[i].lastName);
+        htmlString += STATE.clientData[i].lastName + ", " + STATE.clientData[i].firstName + "<br>";
+    }
+    console.log(STATE.clientData);
+    document.getElementById("view").innerHTML = htmlString;
+}
+
 function invoiceView() {
     let theHtml = "";
     theHtml = `
-    <div class="grid-item info">
-        Invoice:<br>
-        Select Client
-    </div>
-    <div class="grid-item main subQuad">
-        <div class="quad-item">A - G</div>
-        <div class="quad-item">H - N</div>
-        <div class="quad-item">O - W</div>
-        <div class="quad-item">X - Z</div>
-    </div>
-    <div class="grid-item sub-main">
-        GENERIC
-    </div>
-    <div class="grid-item sub-one" onclick="changeView(mainView())">
-        << BACK
-    </div>
-    <div class="grid-item sub-two">
-        .
-    </div>
-    <div class="grid-item sub-three">
-        NEW CLIENT
-    </div>
+        <div class="grid-item info">
+            Invoice:<br>
+            Select Client
+        </div>
+        <div class="grid-item main subQuad">
+            <div class="quad-item" onclick="checkClientData()">A - G</div>
+            <div class="quad-item">H - N</div>
+            <div class="quad-item">O - W</div>
+            <div class="quad-item">X - Z</div>
+        </div>
+        <div class="grid-item sub-main">
+            GENERIC
+        </div>
+        <div class="grid-item sub-one" onclick="changeView(mainView())">
+            << BACK
+        </div>
+        <div class="grid-item sub-two">
+            .
+        </div>
+        <div class="grid-item sub-three">
+            NEW CLIENT
+        </div>
     `;        
     return theHtml;
 }
